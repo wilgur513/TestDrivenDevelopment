c03d08c23500b92402bb3a93f187764ceb440e94
wilgur513 <wilgur513@naver.com>
2020-12-13 12:17:38 +0900

    동적 메서드 실행 테스트 동작하도록 변경

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -3,7 +3,15 @@ package xunit;
 public class WasRun {
     boolean wasRun;
 
+    public WasRun(String name){
+
+    }
+
     public void testMethod(){
         wasRun = true;
+    }
+
+    public void run(){
+
     }
 }
