7e5f343ef9e3b2c9667914d9bbc51fd6a84dcf9f
wilgur513 <wilgur513@naver.com>
2020-12-13 14:31:24 +0900

    assertEquals 다른 값 비교 구현

--- src/xunit/Assert.java
+++ src/xunit/Assert.java
@@ -2,6 +2,7 @@ package xunit;
 
 public class Assert {
     public static void assertEquals(Object actual, Object expected){
-
+        if(!actual.equals(expected))
+            throw new RuntimeException("fail! actual : " + actual + ", expected : " + expected);
     }
 }
--- src/xunit/Main.java
+++ src/xunit/Main.java
@@ -3,7 +3,7 @@ package xunit;
 public class Main {
     public static void main(String[] args) {
         new TestCaseTest("testRunning").run();
-        new TestCaseTest("testAssertEqualsTrueCase").run();
-        new TestCaseTest("testAssertEqualsFalseCase").run();
+        new TestCaseTest("testAssertEqualsEquivalentValues").run();
+        new TestCaseTest("testAssertEqualsNotEquivalentValues").run();
     }
 }
