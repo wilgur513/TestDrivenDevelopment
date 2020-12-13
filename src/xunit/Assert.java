cb9b641a7fbe9e9b6be7bc9f99eb5f3e64ebd935
wilgur513 <wilgur513@naver.com>
2020-12-13 14:27:45 +0900

    assertEquals 동일한 값 비교 테스트 통과

---
+++ src/xunit/Assert.java
@@ -0,0 +1,7 @@
+package xunit;
+
+public class Assert {
+    public static void assertEquals(Object actual, Object expected){
+
+    }
+}
--- src/xunit/Main.java
+++ src/xunit/Main.java
@@ -3,5 +3,6 @@ package xunit;
 public class Main {
     public static void main(String[] args) {
         new TestCaseTest("testRunning").run();
+        new TestCaseTest("testAssertEqualsTrueCase").run();
     }
 }
