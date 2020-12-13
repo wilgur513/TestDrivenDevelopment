0ed946ec83ff918a1c912ff81f38154c7984098f
wilgur513 <wilgur513@naver.com>
2020-12-13 12:13:53 +0900

    첫 번째 테스트 코드 작성

--- src/xunit/Main.java
+++ src/xunit/Main.java
@@ -2,6 +2,9 @@ package xunit;
 
 public class Main {
     public static void main(String[] args) {
-        
+        WasRun wasRun = new WasRun();
+        System.out.println(wasRun.wasRun); // false;
+        wasRun.testMethod();
+        System.out.println(wasRun.wasRun); // true;
     }
 }
