73a53c43ba64721adda23850680ec33345258040
wilgur513 <wilgur513@naver.com>
2020-12-13 14:37:39 +0900

    로그로 setUp 메서드 실행 여부 확인 구현

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -1,6 +1,7 @@
 package xunit;
 
 public class WasRun extends TestCase{
+    String log;
     boolean wasSetUp;
     boolean wasRun;
 
@@ -14,5 +15,6 @@ public class WasRun extends TestCase{
 
     public void setUp(){
         wasSetUp = true;
+        log = "setUp";
     }
 }
