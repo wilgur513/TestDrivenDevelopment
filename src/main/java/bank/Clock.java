71c2c84201fda4aee4c66636809b0dd23726872e
wilgur513 <wilgur513@naver.com>
2021-04-27 23:29:09 +0900

    return_todays_date_in_MM_dd_yyyy_format 테스트 통과

--- src/main/java/bank/Clock.java
+++ src/main/java/bank/Clock.java
@@ -1,7 +1,16 @@
 package bank;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Clock {
+    public static final DateTimeFormatter DD_MM_YYYY = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     public String todayAsString() {
-        return null;
+        return today().format(DD_MM_YYYY);
+    }
+
+    protected LocalDate today() {
+        return LocalDate.now();
     }
 }
--- src/test/java/bank/ClockShould.java
+++ src/test/java/bank/ClockShould.java
@@ -2,14 +2,23 @@ package bank;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ClockShould {
     @Test
     public void return_todays_date_in_dd_MM_yyyy_format() {
-        Clock clock = new Clock();
+        Clock clock = new TestableClock();
         String date = clock.todayAsString();
         assertThat(date, is("24/04/2015"));
+    }
+
+    class TestableClock extends Clock {
+        @Override
+        protected LocalDate today() {
+            return LocalDate.of(2015, 4, 24);
+        }
     }
 }
