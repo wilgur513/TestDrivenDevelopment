45c191f84be3a8cc22f05ac61dcc100af8d1c987
wilgur513 <wilgur513@naver.com>
2021-04-27 23:41:29 +0900

    헤더 문자열 상수화 리팩토링

--- src/main/java/bank/StatementPrinter.java
+++ src/main/java/bank/StatementPrinter.java
@@ -3,6 +3,7 @@ package bank;
 import java.util.List;
 
 public class StatementPrinter {
+    public static final String STATEMENT_HEADER = "DATE | AMOUNT | BALANCE";
     private final Console console;
 
     public StatementPrinter(Console console) {
@@ -10,6 +11,6 @@ public class StatementPrinter {
     }
 
     public void print(List<Transaction> transactions) {
-        console.printLine("DATE | AMOUNT | BALANCE");
+        console.printLine(STATEMENT_HEADER);
     }
 }
