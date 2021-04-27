bb936d3bd9a80fde51544f4704362ef5e851c2e3
wilgur513 <wilgur513@naver.com>
2021-04-27 23:17:03 +0900

    TransactionRepository 테스트 코드 리팩토링

--- src/test/java/bank/TransactionRepositoryShould.java
+++ src/test/java/bank/TransactionRepositoryShould.java
@@ -23,11 +23,11 @@ public class TransactionRepositoryShould {
     @Before
     public void setUp() throws Exception {
         transactionRepository = new TransactionRepository(clock);
+        given(clock.todayAsString()).willReturn(TODAY);
     }
 
     @Test
     public void create_and_store_a_deposit_transaction() {
-        given(clock.todayAsString()).willReturn(TODAY);
         transactionRepository.addDeposit(100);
 
         List<Transaction> transactions = transactionRepository.allTransactions();
@@ -38,7 +38,6 @@ public class TransactionRepositoryShould {
 
     @Test
     public void create_and_store_a_withdrawal_transaction() {
-        given(clock.todayAsString()).willReturn(TODAY);
         transactionRepository.addWithdrawal(100);
 
         List<Transaction> transactions = transactionRepository.allTransactions();
