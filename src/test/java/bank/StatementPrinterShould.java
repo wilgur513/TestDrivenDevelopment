f5202b0007f44ec8ece1f540eb0fe611ad2307fc
wilgur513 <wilgur513@naver.com>
2021-04-27 23:50:20 +0900

    발생 순서대로 트랜잭션 출력 확인하는 테스트 작성

--- src/test/java/bank/StatementPrinterShould.java
+++ src/test/java/bank/StatementPrinterShould.java
@@ -2,9 +2,12 @@ package bank;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,5 +24,32 @@ public class StatementPrinterShould {
         StatementPrinter statementPrinter = new StatementPrinter(console);
         statementPrinter.print(NO_TRANSACTIONS);
         verify(console).printLine("DATE | AMOUNT | BALANCE");
+    }
+
+    @Test
+    public void print_transactions_in_reverse_chronological_order() {
+        StatementPrinter statementPrinter = new StatementPrinter(console);
+        statementPrinter.print(transactionContaining(
+                                    deposit("01/04/2014", 1000),
+                                    withdrawal("02/04/2014", 100),
+                                    deposit("10/04/2014", 500)));
+
+        InOrder inOrder = Mockito.inOrder(console);
+        inOrder.verify(console).printLine("DATE | AMOUNT | BALANCE");
+        inOrder.verify(console).printLine("10/04/2014 | 500.00 | 1400.00");
+        inOrder.verify(console).printLine("02/04/2014 | -100.00 | 900.00");
+        inOrder.verify(console).printLine("01/04/2014 | 1000.00 | 1000.00");
+    }
+
+    private List<Transaction> transactionContaining(Transaction... transactions) {
+        return Arrays.asList(transactions);
+    }
+
+    private Transaction withdrawal(String date, int amount) {
+        return new Transaction(date, amount);
+    }
+
+    private Transaction deposit(String date, int amount) {
+        return new Transaction(date, amount);
     }
 }
