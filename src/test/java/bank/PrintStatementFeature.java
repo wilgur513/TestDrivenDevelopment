452f4e47989ad04fae307d34d3d6c7a9ca1a2f63
wilgur513 <wilgur513@naver.com>
2021-04-27 21:38:51 +0900

    acceptance 테스트 수정

--- src/test/java/bank/PrintStatementFeature.java
+++ src/test/java/bank/PrintStatementFeature.java
@@ -3,7 +3,9 @@ package bank;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.verify;
@@ -25,10 +27,11 @@ public class PrintStatementFeature {
         account.deposit(500);
 
         account.printStatement();
-
-        verify(console).printLine("DATE | AMOUNT | BALANCE");
-        verify(console).printLine("10/04/2014 | 500.00 | 1400.00");
-        verify(console).printLine("02/04/2014 | -100.00 | 900.00");
-        verify(console).printLine("01/04/2014 | 1000.00 | 1000.00");
+        
+        InOrder inOrder = Mockito.inOrder(console);
+        inOrder.verify(console).printLine("DATE | AMOUNT | BALANCE");
+        inOrder.verify(console).printLine("10/04/2014 | 500.00 | 1400.00");
+        inOrder.verify(console).printLine("02/04/2014 | -100.00 | 900.00");
+        inOrder.verify(console).printLine("01/04/2014 | 1000.00 | 1000.00");
     }
 }
