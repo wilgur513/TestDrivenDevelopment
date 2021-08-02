9ea9a67b07ea00e5c998ff3dc75e560770145a21
wilgur513 <wilgur513@naver.com>
2021-08-02 17:52:35 +0900

    Note 단어 하나만 가진 문자열 테스트 통과
    str과 "note"를 비교해 동일한 경우 "{note}"
    다른 경우 str을 반환

--- src/main/java/highlight/Highlighter.java
+++ src/main/java/highlight/Highlighter.java
@@ -2,6 +2,10 @@ package highlight;
 
 public class Highlighter {
     public String highlight(String str) {
+        if(str.equals("note")) {
+            return "{note}";
+        }
+
         return str;
     }
 }
