06cb8c44a532e2688c287b4bd5c17feae7a46924
wilgur513 <wilgur513@naver.com>
2021-08-02 18:05:54 +0900

    중요 표시한 "{note}" 문자열을 반환하는 메서드 추출
    PRE_MARKER와 POST_MARKER를 사용해 "{", "}" 표현
    markedNote 메서드에서 PRE_MARKER, TARGET, POST_MARKER를 사용해 "{note}" 반환

--- src/main/java/highlight/Highlighter.java
+++ src/main/java/highlight/Highlighter.java
@@ -2,14 +2,16 @@ package highlight;
 
 public class Highlighter {
     private static final String TARGET = "note";
+    private static final String PRE_MARKER = "{";
+    private static final String POST_MARKER = "}";
 
     public String highlight(String str) {
         if(noteIndex(str) < 0) {
             return str;
         }
 
-        String result = str.substring(0, noteIndex(str)) + "{note}";
-
+        String result = str.substring(0, noteIndex(str)) + markedNote();
+        
         if(noteIndex(str) + TARGET.length() > str.length()) {
             return result;
         }
@@ -19,6 +21,10 @@ public class Highlighter {
 
     private int noteIndex(String str) {
         return str.indexOf(TARGET);
+    }
+
+    private String markedNote() {
+        return PRE_MARKER + TARGET + POST_MARKER;
     }
 
 
