0a79b5e3d4eaff90cee574ccccb38c0edfafd9bc
wilgur513 <wilgur513@naver.com>
2021-08-02 17:58:23 +0900

    "note" 단어 하나와 다른 단어들의 조합으로 만들어진 문자열 테스트 통과
    str 문자열에서 indexOf로 index 값을 확인하여 찾은 "note" 값을 "{note}"로 변환하고
    "note" 앞, 뒤의 문자열을 붙여서 결과로 만들어 반환

--- src/main/java/highlight/Highlighter.java
+++ src/main/java/highlight/Highlighter.java
@@ -2,10 +2,18 @@ package highlight;
 
 public class Highlighter {
     public String highlight(String str) {
-        if(str.equals("note")) {
-            return "{note}";
+        int index = str.indexOf("note");
+
+        if(index < 0) {
+            return str;
         }
 
-        return str;
+        String result = str.substring(0, index) + "{note}";
+
+        if(index + "note".length() > str.length()) {
+            return result;
+        }
+
+        return result + str.substring(index + "note".length());
     }
 }
