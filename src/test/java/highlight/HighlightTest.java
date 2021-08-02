a08c4e6f7acd2894d3d295e61490a8e80611d878
wilgur513 <wilgur513@naver.com>
2021-08-02 17:54:44 +0900

    "note" 단어 하나와 다른 단어들과 조합된 문자열 테스트
    - a note -> a {note}

--- src/test/java/highlight/HighlightTest.java
+++ src/test/java/highlight/HighlightTest.java
@@ -1,20 +1,31 @@
 package highlight;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HighlightTest {
+    private Highlighter highlighter;
+
+    @Before
+    public void setUp() throws Exception {
+        highlighter = new Highlighter();
+    }
+
     @Test
     public void notHasNoteString() {
-        Highlighter highlighter = new Highlighter();
         assertThat(highlighter.highlight("abc")).isEqualTo("abc");
         assertThat(highlighter.highlight("asdf")).isEqualTo("asdf");
     }
 
     @Test
     public void onlyNoteString() {
-        Highlighter highlighter = new Highlighter();
         assertThat(highlighter.highlight("note")).isEqualTo("{note}");
+    }
+
+    @Test
+    public void hasOneNoteString() {
+         assertThat(highlighter.highlight("a note")).isEqualTo("a {note}");
     }
 }
