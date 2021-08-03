f360252a0434a8926773b37f3efb1bf5fd24542f
wilgur513 <wilgur513@naver.com>
2021-08-03 22:41:03 +0900

    splitHasOnlyOneNote 테스트 통과
    HighlightSplitter 구현
     - next : str을 현재 타겟 뒤의 문자열로 변경 및 변경된 문자열의 타겟 위치 계산
     - hasNext : index가 0 이상인 경우 다음 타겟이 존재
     - prevTargetString : 현재 타겟의 위치인 index 값을 사용해 타겟 이전의 문자열 반환
     - postTargetString : 현재 타겟의 위치인 index 값과 target의 길이를 이용해 타겟 뒤의 문자열 반환
     - splitter : str과 target 을 이용해 HighlightSplitter 객체 생성

--- src/main/java/highlight/HighlightSplitter.java
+++ src/main/java/highlight/HighlightSplitter.java
@@ -1,23 +1,38 @@
 package highlight;
 
 public class HighlightSplitter {
-    public static HighlightSplitter splitter(String str, String target) {
-        return null;
+    private String str;
+    private final String target;
+    private int index;
+
+    private HighlightSplitter(String str, String target) {
+        this.str = str;
+        this.target = target;
+        this.index = str.indexOf(target);
     }
 
     public void next() {
-
+        str = str.substring(index + target.length());
+        index = str.indexOf(target);
     }
 
     public boolean hasNext() {
-        return false;
+        return index >= 0;
     }
 
     public String prevTargetString() {
-        return null;
+        return str.substring(0, index);
     }
 
     public String postTargetString() {
-        return null;
+        if(index + target.length() > str.length()) {
+            return "";
+        }
+
+        return str.substring(index + target.length());
+    }
+
+    public static HighlightSplitter splitter(String str, String target) {
+        return new HighlightSplitter(str, target);
     }
 }
