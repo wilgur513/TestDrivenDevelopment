f6ba5d8cef694315602e02bff4acd5e6ce72de08
wilgur513 <wilgur513@naver.com>
2021-08-03 23:44:15 +0900

    splitHasMoreTarget 테스트 작성
    Target 단어가 여러개 들어간 문자열 split 하기

--- src/test/java/highlight/HighlightSplitterTest.java
+++ src/test/java/highlight/HighlightSplitterTest.java
@@ -24,4 +24,22 @@ public class HighlightSplitterTest {
          HighlightSplitter splitter = HighlightSplitter.splitter("note note", TARGET);
          splitter.postTargetString();
     }
+
+    @Test
+    public void splitHasMoreTarget() {
+        String[] expectedPrevTargetString = {"", " ", " key "};
+        String expectedPostTargetString = " a b c";
+        String str = "note note key note a b c";
+
+        HighlightSplitter splitter = HighlightSplitter.splitter(str, TARGET);
+
+        for(int i = 0; i < 3; i++) {
+            assertThat(splitter.hasNext()).isTrue();
+            assertThat(splitter.prevTargetString()).isEqualTo(expectedPrevTargetString[i]);
+            splitter.next();
+        }
+
+        assertThat(splitter.hasNext()).isFalse();
+        assertThat(splitter.postTargetString()).isEqualTo(expectedPostTargetString);
+    }
 }
