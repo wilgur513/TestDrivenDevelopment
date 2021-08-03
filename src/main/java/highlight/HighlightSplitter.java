dfbeb6a4cd71fd3100a395d6083ebe24f22652cd
wilgur513 <wilgur513@naver.com>
2021-08-03 22:33:41 +0900

    "note" 단어 하나만 가진 문자열 split 하기
    HighlightSpliiter 객체를 사용해 문자열을 타겟 단어 기준으로 쪼개기
    - splitter : 문자열과 타겟 단어를 사용해 HighlightSplitter 객체 생성
    - hasNext : 다음 타겟 존재 여부 확인
    - next : 다음 타겟으로 이동
    - prevTargetString : 현재 타겟과 이전 타겟 사이의 문자열 반환
    - postTargetString : 현재 타겟 뒤의 문자열을 반환

---
+++ src/main/java/highlight/HighlightSplitter.java
@@ -0,0 +1,23 @@
+package highlight;
+
+public class HighlightSplitter {
+    public static HighlightSplitter splitter(String str, String target) {
+        return null;
+    }
+
+    public void next() {
+
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public String prevTargetString() {
+        return null;
+    }
+
+    public String postTargetString() {
+        return null;
+    }
+}
---
+++ src/test/java/highlight/HighlightSplitterTest.java
@@ -0,0 +1,19 @@
+package highlight;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HighlightSplitterTest {
+    @Test
+    public void splitHasOnlyOneNote() {
+        HighlightSplitter splitter = HighlightSplitter.splitter("note", "note");
+
+        assertThat(splitter.hasNext()).isTrue();
+        assertThat(splitter.prevTargetString()).isEqualTo("");
+
+        splitter.next();
+        assertThat(splitter.hasNext()).isFalse();
+        assertThat(splitter.postTargetString()).isEqualTo("");
+    }
+}
